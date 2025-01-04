2cec8605f0bac4bba501bd4300ffe56d5d56ea4b
Aurelien Euillot <a.euillot@pm.me>
2025-01-04 09:38:07 +0000

    feat: stylize posts show

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,12 +2,12 @@ module PostsHelper
   def blog_post_life_cycle_options(post)
     case post.status.to_sym
     when :draft
-      button_to "Publish this post", post, method: :patch, params: { post: { status: :published } }, class: "mt-2 rounded-lg py-3 px-5 bg-gray-100 font-medium"
+      button_to "Publish this post", post, method: :patch, params: { post: { status: :published } }, class: "block text-base text-gray-600 font-normal hover:text-gray-900"
     when :published
-      button_to("Broadcast this post", post_broadcasts_path(post), method: :post, class: "mt-2 rounded-lg py-3 px-5 bg-gray-100 font-medium") +
-      button_to("Archive this post", post, method: :patch, params: { post: { status: :archived } }, class: "mt-2 rounded-lg py-3 px-5 bg-gray-100 font-medium")
+      button_to("Broadcast this post", post_broadcasts_path(post), method: :post, class: "block text-base text-gray-600 font-normal hover:text-gray-900") +
+      button_to("Archive this post", post, method: :patch, params: { post: { status: :archived } }, class: "block text-base text-gray-600 font-normal hover:text-gray-900")
     when :archived
-      button_to "Destroy this post", post, method: :delete, class: "mt-2 rounded-lg py-3 px-5 bg-gray-100 font-medium"
+      button_to "Destroy this post", post, method: :delete, class: "block text-base text-gray-600 font-normal hover:text-gray-900"
     end
   end
 end
