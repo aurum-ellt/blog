610f089b723d95032150023c83d046181cfc72dd
Aurelien Euillot <a.euillot@pm.me>
2025-01-06 10:55:00 +0000

    fix(post): update life cycle options

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,13 +1,17 @@
 module PostsHelper
   def blog_post_life_cycle_options(post)
-    case post.status.to_sym
-    when :draft
-      button_to "Publish this post", post, method: :patch, params: { post: { status: :published } }, class: "block text-base text-gray-600 font-normal hover:text-gray-900"
-    when :published
-      button_to("Broadcast this post", post_broadcasts_path(post), method: :post, class: "block text-base text-gray-600 font-normal hover:text-gray-900") +
-      button_to("Archive this post", post, method: :patch, params: { post: { status: :archived } }, class: "block text-base text-gray-600 font-normal hover:text-gray-900")
-    when :archived
-      button_to "Destroy this post", post, method: :delete, class: "block text-base text-gray-600 font-normal hover:text-gray-900"
+    options = ""
+
+    if post.draft? || post.archived?
+      options += button_to("Publish this post", post, method: :patch, params: { post: { status: :published } }, class: "block text-base text-gray-600 font-normal hover:text-gray-900")
+      options += button_to("Destroy this post", post, method: :delete, class: "block text-base text-gray-600 font-normal hover:text-gray-900")
     end
+
+    if post.published?
+      options += button_to("Broadcast this post", post_broadcasts_path(post), method: :post, class: "block text-base text-gray-600 font-normal hover:text-gray-900")
+      options += button_to("Archive this post", post, method: :patch, params: { post: { status: :archived } }, class: "block text-base text-gray-600 font-normal hover:text-gray-900")
+    end
+
+    options.html_safe
   end
 end
