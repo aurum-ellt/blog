7a625a8ff8a2e0f2c14710e7a9ca334241d8ff01
Aurelien Euillot <a.euillot@pm.me>
2025-01-07 15:44:10 +0000

    chore: some style changes

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,13 +3,13 @@ module PostsHelper
     options = ""
 
     if post.draft? || post.archived?
-      options += button_to("Publish this post", post, method: :patch, params: { post: { status: :published } }, class: "block text-base text-gray-600 font-normal hover:text-gray-900")
-      options += button_to("Destroy this post", post, method: :delete, class: "block text-base text-gray-600 font-normal hover:text-gray-900")
+      options += button_to("Publish this post", post, method: :patch, params: { post: { status: :published } }, class: "block text-base text-gray-500 font-normal hover:text-primary-500 hover:font-semibold")
+      options += button_to("Destroy this post", post, method: :delete, class: "block text-base text-gray-500 font-normal hover:text-primary-500 hover:font-semibold")
     end
 
     if post.published?
-      options += button_to("Broadcast this post", post_broadcasts_path(post), method: :post, class: "block text-base text-gray-600 font-normal hover:text-gray-900")
-      options += button_to("Archive this post", post, method: :patch, params: { post: { status: :archived } }, class: "block text-base text-gray-600 font-normal hover:text-gray-900")
+      options += button_to("Broadcast this post", post_broadcasts_path(post), method: :post, class: "block text-base text-gray-500 font-normal hover:text-primary-500 hover:font-semibold")
+      options += button_to("Archive this post", post, method: :patch, params: { post: { status: :archived } }, class: "block text-base text-gray-500 font-normal hover:text-primary-500 hover:font-semibold")
     end
 
     options.html_safe
