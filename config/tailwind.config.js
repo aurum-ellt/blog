e8fab0ddb60a0533a305ba035acfb4064e138fd9
Aurelien Euillot <a.euillot@pm.me>
2025-01-07 11:17:43 +0000

    feat: add minimalist design system

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,7 +12,17 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-    },
+      colors: {
+        primary: {
+          100: '#dfe8e6',
+          400: '#71908b',
+          500: '#577571',
+          600: '#445d49',
+          700: '#394c49',
+          800: '#313e3e',
+        }
+      }
+    }
   },
   plugins: [
     // require('@tailwindcss/forms'),
