0dd07647fd557d73f948c1cf96d14c2adec3ebbf
Aurelien Euillot <a.euillot@pm.me>
2025-01-09 09:42:50 +0000

    feat: add home page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "posts/drafts#index"
+  root "pages#home"
 
   resource :registration, only: %i[ new create ]
   resource :session
