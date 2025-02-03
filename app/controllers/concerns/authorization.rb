4615c59cae179d850841de7273250e5a40bf659a
Aurelien Euillot <a.euillot@pm.me>
2025-02-03 14:59:09 +0000

    feat: restrict jobs monitoring to admin

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -2,6 +2,10 @@ module Authorization
   extend ActiveSupport::Concern
 
   private
+    def ensure_can_administer
+      head :forbidden unless Current.user.admin?
+    end
+
     def ensure_permissions_for(resource)
       head :forbidden unless resource.available?
     end
