5bbe8d249d78d1a6ea8f6730193baef7358a78ab
Aurelien Euillot <a.euillot@pm.me>
2025-03-07 17:06:09 +0100

    chore: rails app update

--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_20_100840) do
+ActiveRecord::Schema[8.1].define(version: 2025_03_07_155904) do
+  create_table "_litestream_lock", id: false, force: :cascade do |t|
+    t.integer "id"
+  end
+
+  create_table "_litestream_seq", force: :cascade do |t|
+    t.integer "seq"
+  end
+
   create_table "action_text_rich_texts", force: :cascade do |t|
-    t.string "name", null: false
     t.text "body"
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.datetime "updated_at", null: false
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -50,65 +58,65 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_20_100840) do
   end
 
   create_table "identities", force: :cascade do |t|
-    t.string "provider_uid", null: false
-    t.string "provider_name", null: false
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
+    t.string "provider_name", null: false
+    t.string "provider_uid", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_identities_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title", limit: 89, null: false
     t.text "body"
-    t.string "slug", null: false
-    t.integer "user_id", null: false
-    t.integer "status", null: false
-    t.datetime "published_at"
     t.datetime "broadcasted_at"
     t.datetime "created_at", null: false
+    t.datetime "published_at"
+    t.string "slug", null: false
+    t.integer "status", null: false
+    t.string "title", limit: 89, null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["published_at", "id"], name: "index_posts_on_published_at_and_id"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "reportings", force: :cascade do |t|
-    t.string "url", null: false
     t.integer "category", null: false
-    t.text "message"
     t.datetime "created_at", null: false
+    t.text "message"
     t.datetime "updated_at", null: false
+    t.string "url", null: false
   end
 
   create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "ip_address"
-    t.string "user_agent"
     t.datetime "created_at", null: false
+    t.string "ip_address"
     t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "subscribers", primary_key: ["email_address", "user_id"], force: :cascade do |t|
-    t.string "email_address", null: false
-    t.integer "user_id", null: false
-    t.string "token", null: false
     t.boolean "confirmed", default: false, null: false
     t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "token", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["token"], name: "index_subscribers_on_token", unique: true
     t.index ["user_id"], name: "index_subscribers_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email_address", null: false
-    t.string "password_digest", null: false
-    t.string "name", limit: 34
-    t.string "slug", null: false
     t.text "bio"
-    t.integer "role", null: false
     t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "name", limit: 34
+    t.string "password_digest", null: false
+    t.integer "role", null: false
+    t.string "slug", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
